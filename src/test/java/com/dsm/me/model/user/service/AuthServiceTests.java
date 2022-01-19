b157842f2523e5222324055bb0e5a54472f6bae6
joungeun <lje0818ovo@naver.com>
2022-01-19 13:59:50 +0900

    👀: Service join test code

--- src/test/java/com/dsm/me/model/user/service/AuthServiceTests.java
+++ src/test/java/com/dsm/me/model/user/service/AuthServiceTests.java
@@ -1,4 +1,49 @@
 package com.dsm.me.model.user.service;
 
+import com.dsm.me.global.exception.EmailOverlapException;
+import com.dsm.me.model.user.dto.UserCreateRequestDto;
+import com.dsm.me.model.user.model.User;
+import com.dsm.me.model.user.model.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+
+@ExtendWith(MockitoExtension.class)
 public class AuthServiceTests {
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private AuthService authService;
+
+    @Test
+    @DisplayName("Join Success")
+    public void join(){
+        UserCreateRequestDto userCreateDto = new UserCreateRequestDto("email@naver","password1!", "nickname");
+
+        given(userRepository.existsById(any())).willReturn(false);
+        given(userRepository.save(any())).willReturn(User.builder().build());
+
+        authService.join(userCreateDto);
+    }
+
+    @Test
+    @DisplayName("Join Failed - Email overlap")
+    public void emailOverlapJoinFail(){
+        UserCreateRequestDto userCreateDto = new UserCreateRequestDto("email@naver","password1!", "nickname");
+
+        given(userRepository.existsById(any())).willReturn(true);
+
+        assertThrows(EmailOverlapException.class, ()->
+            authService.join(userCreateDto)
+        );
+    }
 }
