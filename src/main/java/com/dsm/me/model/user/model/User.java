26869ab9bbd07653b57baa6b315b092699e52d3e
joungeun <lje0818ovo@naver.com>
2022-01-19 15:16:13 +0900

    🦋 #1: Add User entity field

--- src/main/java/com/dsm/me/model/user/model/User.java
+++ src/main/java/com/dsm/me/model/user/model/User.java
@@ -1,7 +1,26 @@
 package com.dsm.me.model.user.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 @Builder
 public class User {
+    @Id
+    private String email;
+    private String password;
+    private String nickname;
+    private String id;
+    private String profile;
+    private String backgroundHax;
+
+    public static String createRandomId(){
+        return "";
+    }
 }
