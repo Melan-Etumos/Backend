0083f1cd624e06e06dcf212ad4019c810408bfc1
joungeun <lje0818ovo@naver.com>
2022-01-19 15:17:29 +0900

    🦋 #1: Add Join method content to AuthService

--- src/main/java/com/dsm/me/model/user/service/AuthService.java
+++ src/main/java/com/dsm/me/model/user/service/AuthService.java
@@ -1,11 +1,37 @@
 package com.dsm.me.model.user.service;
 
+import com.dsm.me.global.exception.EmailOverlapException;
 import com.dsm.me.model.user.dto.UserCreateRequestDto;
+import com.dsm.me.model.user.model.User;
+import com.dsm.me.model.user.model.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class AuthService {
-    public void join(UserCreateRequestDto userCreateDto){
+    @Value("${user.profile}")
+    private final String profile;
+    @Value("${user.hax}")
+    private final String backgroundHax;
 
+    private final UserRepository userRepository;
+
+    public void join(UserCreateRequestDto userCreateDto){
+        if(userRepository.existsById(userCreateDto.getEmail())){
+            throw new EmailOverlapException();
+        }
+
+        userRepository.save(
+                User.builder()
+                        .email(userCreateDto.getEmail())
+                        .password(userCreateDto.getPassword())
+                        .nickname(userCreateDto.getNickname())
+                        .id(User.createRandomId())
+                        .profile(profile)
+                        .backgroundHax(backgroundHax)
+                        .build()
+        );
     }
 }
