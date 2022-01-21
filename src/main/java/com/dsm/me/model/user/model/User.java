125d1f08d81133586aecaa75be7a57b4e547226c
joungeun <lje0818ovo@naver.com>
2022-01-21 12:39:47 +0900

    🦋 #3: Random password generation method

--- src/main/java/com/dsm/me/model/user/model/User.java
+++ src/main/java/com/dsm/me/model/user/model/User.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.util.UUID;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -22,5 +23,18 @@ public class User {
 
     public static String createRandomId(){
         return "";
+    }
+
+    public static String createRandomPassword(){
+        String uid = UUID.randomUUID().toString().replaceAll("-","");
+        return uid.substring(0,14)+randomSpecialChar();
+    }
+
+    private static String randomSpecialChar(){
+        String[] scArr = new String[]{
+                "!","@","#","$","%","^","&","*"
+        };
+        int selectRandomInt = (int) (Math.random()*(scArr.length));
+        return scArr[selectRandomInt];
     }
 }
