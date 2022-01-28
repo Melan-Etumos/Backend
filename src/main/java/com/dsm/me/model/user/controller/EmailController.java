d610db4304f9651bcf85c483cb624f8c9208e858
joungeun <lje0818ovo@naver.com>
2022-01-29 00:23:54 +0900

    😈 #5: Add @RestController... I'm silly

--- src/main/java/com/dsm/me/model/user/controller/EmailController.java
+++ src/main/java/com/dsm/me/model/user/controller/EmailController.java
@@ -1,19 +1,22 @@
 package com.dsm.me.model.user.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.Email;
+
+@RestController
 @RequestMapping("/email")
 public class EmailController {
     @Value("${email.send}")
     private String sendFrom;
 
-    @GetMapping("/{email}")
-    public void sendEmailCode(@PathVariable String email){
+    @GetMapping
+    public void sendEmailCode(@RequestParam @Email String email){
         String title = "Melan Etumos 회원가입 인증 이메일";
-
-
     }
 }
