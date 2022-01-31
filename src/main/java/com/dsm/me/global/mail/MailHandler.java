8e2de3fdcc66a7fd578e75d8a0ec340b127e8533
joungeun <lje0818ovo@naver.com>
2022-01-31 14:22:41 +0900

    🦋 #5: Add method to MailHandler

--- src/main/java/com/dsm/me/global/mail/MailHandler.java
+++ src/main/java/com/dsm/me/global/mail/MailHandler.java
@@ -1,6 +1,5 @@
 package com.dsm.me.global.mail;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -14,13 +13,24 @@ public class MailHandler {
     @Value("${email.send}")
     private String sendFrom;
 
-    private final JavaMailSender mailSender;
-    private final MimeMessage message;
+//    private final JavaMailSender mailSender;
     private final MimeMessageHelper messageHelper;
 
     public MailHandler(JavaMailSender mailSender) throws MessagingException {
-        this.mailSender = mailSender;
-        this.message = mailSender.createMimeMessage();
+//        this.mailSender = mailSender;
+        MimeMessage message = mailSender.createMimeMessage();
         this.messageHelper = new MimeMessageHelper(message, true, "UTF-8");
+    }
+
+    // 이 메서드에서 보내는 사람과 받는 사람 지정
+    public void setSenderAndReceiver(MailReceiver receiver) throws MessagingException {
+        messageHelper.setFrom(sendFrom);
+        messageHelper.setTo(receiver.email);
+    }
+
+    // 이 메서드에서 메세지 지정
+    public void setMailMessage(MailContent content) throws MessagingException {
+        messageHelper.setSubject(content.title);
+        messageHelper.setText(content.content, true);
     }
 }
