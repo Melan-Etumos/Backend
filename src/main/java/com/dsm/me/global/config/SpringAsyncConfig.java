7751b16455e2133cc76bd290a43f80159bf86951
joungeun <lje0818ovo@naver.com>
2022-02-04 21:24:38 +0900

    🦋 #5: SpringAsyncConfig Class

---
+++ src/main/java/com/dsm/me/global/config/SpringAsyncConfig.java
@@ -0,0 +1,9 @@
+package com.dsm.me.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class SpringAsyncConfig {
+}
