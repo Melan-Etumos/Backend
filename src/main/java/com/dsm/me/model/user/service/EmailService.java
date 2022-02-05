9fca38f9d245ad13364f0ba36fee0f5c4fb3a8dc
joungeun <lje0818ovo@naver.com>
2022-02-05 22:17:02 +0900

    🦋 #9 #10: Email code save logic

--- src/main/java/com/dsm/me/model/user/service/EmailService.java
+++ src/main/java/com/dsm/me/model/user/service/EmailService.java
@@ -3,6 +3,8 @@ package com.dsm.me.model.user.service;
 import com.dsm.me.global.mail.MailContent;
 import com.dsm.me.global.mail.MailHandler;
 import com.dsm.me.global.mail.MailReceiver;
+import com.dsm.me.model.user.model.redis.Code;
+import com.dsm.me.model.user.model.redis.EmailCodeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
@@ -17,24 +19,27 @@ import java.util.Random;
 public class EmailService {
     private final MailHandler mailHandler;
     private final SpringTemplateEngine templateEngine;
+    private final EmailCodeRepository codeRepository;
 
     @Async
-    public void sendEmailCode(String email) throws MessagingException {
+    public void sendEmailCode(final String email) throws MessagingException {
         mailHandler.setSenderAndReceiver(MailReceiver.builder().email(email).build());
-        mailHandler.setMailContent(createMailContent());
+        mailHandler.setMailContent(createMailContent(email));
         mailHandler.sendMail();
     }
 
-    public MailContent createMailContent(){
+    public MailContent createMailContent(String email){
         final String title = "Melan Etumos 회원가입 인증 메일입니다.";
 
         Context context = new Context();
-        context.setVariable("code",createEmailCode());
+        context.setVariable("code",createEmailCodeAndSave(email));
         return MailContent.builder().content(templateEngine.process("email_code", context)).title(title).build();
     }
 
-    private String createEmailCode(){
+    private String createEmailCodeAndSave(String email){
         Random random = new Random();
-        return Integer.toString(random.nextInt(999999));
+        String code = Integer.toString(random.nextInt(999999));
+        codeRepository.save(Code.builder().email(email).code(code).build());
+        return code;
     }
 }
