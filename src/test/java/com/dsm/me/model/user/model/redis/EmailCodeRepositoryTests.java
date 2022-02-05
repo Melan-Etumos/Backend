3326452c6177e5c35bb848578bdcf096caef104a
joungeun <lje0818ovo@naver.com>
2022-02-06 04:18:39 +0900

    👀 #9: EmailCodeRepository save test

---
+++ src/test/java/com/dsm/me/model/user/model/redis/EmailCodeRepositoryTests.java
@@ -0,0 +1,23 @@
+package com.dsm.me.model.user.model.redis;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+public class EmailCodeRepositoryTests {
+    @Autowired
+    EmailCodeRepository emailCodeRepository;
+
+    @Test
+    public void saveTest(){
+        final String code = "111111";
+        Code entity = emailCodeRepository.save(Code.builder().email("test@naver.com").code(code).build());
+        assertEquals(code, entity.getCode());
+    }
+}
