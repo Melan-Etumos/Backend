3f3408d451639975cf7131c919f4559997d57fb0
joungeun <lje0818ovo@naver.com>
2022-02-08 02:13:32 +0900

    👀 #9: Email verification code check repository tests

--- src/test/java/com/dsm/me/model/user/model/redis/EmailCodeRepositoryTests.java
+++ src/test/java/com/dsm/me/model/user/model/redis/EmailCodeRepositoryTests.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
@@ -19,5 +19,26 @@ public class EmailCodeRepositoryTests {
         final String code = "111111";
         Code entity = emailCodeRepository.save(Code.builder().email("test@naver.com").code(code).build());
         assertEquals(code, entity.getCode());
+    }
+
+    @Test
+    public void existCodeTest(){
+        saveTest();
+        boolean check = emailCodeRepository.existsByEmailAndCode("test@naver.com", "111111");
+        assertTrue(check);
+    }
+
+    @Test
+    public void existCodeEmailFailTest(){
+        saveTest();
+        boolean check = emailCodeRepository.existsByEmailAndCode("test.com", "111111");
+        assertFalse(check);
+    }
+
+    @Test
+    public void existCodeDifferFailTest(){
+        saveTest();
+        boolean check = emailCodeRepository.existsByEmailAndCode("test@naver.com", "000000");
+        assertFalse(check);
     }
 }
