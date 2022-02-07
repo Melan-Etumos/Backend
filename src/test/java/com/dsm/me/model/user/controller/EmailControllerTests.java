08c6b87d20a9821195bacb7dbaf27c040b1a2aaa
joungeun <lje0818ovo@naver.com>
2022-02-08 02:09:08 +0900

    👀 #9: Email verification code check success controller test

--- src/test/java/com/dsm/me/model/user/controller/EmailControllerTests.java
+++ src/test/java/com/dsm/me/model/user/controller/EmailControllerTests.java
@@ -6,9 +6,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = EmailController.class)
@@ -31,6 +33,30 @@ public class EmailControllerTests {
     public void sendEmailFailedTest() throws Exception {
         final String email = "test.com";
         mvc.perform(get("/email").param("email", email))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("email verification code check success test")
+    public void emailCodeCheckTest() throws Exception {
+        final String email = "test@naver.com";
+
+        mvc.perform(post("/email/{email}",email)
+                .content("000000")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("email verification code check fail test")
+    public void emailCodeCheckFailTest() throws Exception {
+        final String email = "test.com";
+
+        mvc.perform(post("/email/{email}",email)
+                .content("000000")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest());
     }
 }
