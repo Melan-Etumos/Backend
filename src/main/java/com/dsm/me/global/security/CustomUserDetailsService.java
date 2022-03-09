fdb3baa599421efeda94135e3a6f6f5f2c603d04
joungeun <lje0818ovo@naver.com>
2022-03-09 15:17:04 +0900

    🦋 #15: @Service to CustomUserDetailsService

--- src/main/java/com/dsm/me/global/security/CustomUserDetailsService.java
+++ src/main/java/com/dsm/me/global/security/CustomUserDetailsService.java
@@ -5,7 +5,9 @@ import com.dsm.me.model.user.model.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
 
+@Service
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
     private final MemberRepository memberRepository;
