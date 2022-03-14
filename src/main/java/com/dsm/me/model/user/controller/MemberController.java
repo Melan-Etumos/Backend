45ef11b670c884ba359f0f9389ff5bb02e7a75ca
joungeun <lje0818ovo@naver.com>
2022-03-14 23:10:31 +0900

    🦋 #15: AuthenticationFacade

--- src/main/java/com/dsm/me/model/user/controller/MemberController.java
+++ src/main/java/com/dsm/me/model/user/controller/MemberController.java
@@ -1,15 +1,25 @@
 package com.dsm.me.model.user.controller;
 
+import com.dsm.me.global.security.AuthenticationFacade;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.constraints.Email;
 
+@RequiredArgsConstructor
 @RestController
 public class MemberController {
+    private final AuthenticationFacade authenticationFacade;
+
     @GetMapping("/user/{email}")
     public void userPage(@PathVariable @Email  String email){
+
+    }
+
+    @GetMapping("/user")
+    public void myPage(){
 
     }
 }
